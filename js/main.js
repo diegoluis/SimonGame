789d94f211655bc44b2988ed9f041707e8b43cd5
diegoluis <diego@diegoluis.net>
2016-07-19 20:50:53 +0200

    first attempt at reproducing all the array of the machine

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*
+- ir generando un array con números del uno al 4, uno cada vez, hasta llegar a 20
+- vincular cada sonido a cada botón
+- reproducir el array una vez con una duración determinada y marcar la vuelta en el contador. A medida que aumenta el número de vueltas la velocidad de reproducción aumenta
+- un programa que almacene las jugadas del jugador y las va comparando con el array, si son correctas vuelve a ejecutar el generador de arrays con un elemento más y una vuelta más.
+- si son incorrectas, revisar si está el "strict mode" activado, si es así vuelve a empezar, si no es así, repite la secuencia.
+- cuando la máquina está jugando no permitir que los botones se puedan presionar por el jugador
+*/
+var gameMachine = {};
+var gamePlayer = {};
+
+function createGMachine(){
+  
+}
