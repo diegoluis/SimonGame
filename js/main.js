c9ab9e68539cf83e02d0ce5029e084e127cb5a94
diegoluis <diego@diegoluis.net>
2016-08-22 20:17:40 +0200

    adjusting strict mode

--- js/main.js
+++ js/main.js
@@ -1,27 +1,28 @@
 /*
------ ir generando un array con números del uno al 4, uno cada vez, hasta llegar a 20
------ vincular cada sonido a cada botón
------ hacer que los botones se ejecuten con luz y sonido de acuerdo al array generado
------ hacer que los botones se apaguen antes de que se reproduzca el siguiente sonido
-- reproducir el array una vez con una duración determinada y marcar la vuelta en el contador. A medida que aumenta el número de vueltas la velocidad de reproducción aumenta
-- reproducir el primer numero y esperar input del jugador
-- un programa que almacene las jugadas del jugador y las va comparando con el array, si son correctas vuelve a ejecutar el generador de arrays con un elemento más y una vuelta más.
+
 - si son incorrectas, revisar si está el "strict mode" activado, si es así vuelve a empezar, si no es así, repite la secuencia.
-- cuando la máquina está jugando no permitir que los botones se puedan presionar por el jugador
+- cuando la máquina está jugando no permitir que los botones se puedan presionar por el jugador (ya está con start)
+-revisar el css (espacios, etc)
+//manejo de errores
+- hacer una secuencia especial cuando gane
+
 */
 //-----------variables-----------//
+var url = window.location.href;
 var gameMachine = [];
-var gamePlayer = [];
+//var gamePlayer = [];
 //store the pushed button from player
 var pushedBtn;
 var number;
 var start = document.getElementById("start");
 var strict = document.getElementById("strict");
+var reset = document.getElementById("reset");
 //buttons
 var green = document.getElementById("green");
 var red = document.getElementById("red");
 var yellow = document.getElementById("yellow");
 var blue = document.getElementById("blue");
+var allButtons = [green,red,yellow,blue];
 //button container
 var boardInt = document.getElementById("boardInt");
 //audios
@@ -37,13 +38,26 @@ var lengthArray;
 // counter on the board
 var counterBoard = document.getElementById("counter");
 //variable to control the steps of the game
-var counterMachine = 2;
+var counterMachine = 1;
+var counter = 0;
 //variable to control the internal steps of each loop
 var loop = 0;
+//variable to control the strict mode
+var strictValue = false;
+/*
+var greenGlow = document.getElementsByClassName("greenGlow");
+var redGlow = document.getElementsByClassName("redGlow");
+var yellowGlow = document.getElementsByClassName("yellowGlow");
+var blueGlow = document.getElementsByClassName("blueGlow");
+*/
 //------------------------------//
 
 //create the random number and add it to an array
 function createGMachine(){
+  //gameMachine = [];
+  //counterMachine = 1;
+  //counter = 0;
+  //loop = 0;
   // generate the sequence of the game of the machine
   for(var i= 0; i<20; i++){
     //execute the random number creator
@@ -52,6 +66,7 @@ function createGMachine(){
     gameMachine.push(number);
     console.log(gameMachine);
   }
+  readGameMachine(counter);
   steps();
 }
 //get a random number
@@ -73,11 +88,20 @@ red.addEventListener("click", function(){playSound(audio2);});
 yellow.addEventListener("click", function(){playSound(audio3);});
 blue.addEventListener("click", function(){playSound(audio4);});
 
-
+//activate/deactivate the buttons
+///NOT WORKING////
+function toggleButtons(action){
+  green.disabled = true;
+  allButtons.forEach(function(e){
+    e.disabled = action;
+    console.log(e);
+  })
+}
 //---- reproduce automatically the array -------//
 timer = 1000;
 //associate each number with a button and a sound
 function playByNumber(number){
+  increaseSpeed(timer);
   switch (number) {
     case 1:
       playSound(audio1);
@@ -105,7 +129,7 @@ function changeClasses(nameId, nameClass, time){
   var timeoutId;
   //function to start the timer and a function to remove the class
   function delayRemove(){
-    timeoutId = window.setTimeout(removeC, time/1.5);
+    timeoutId = window.setTimeout(removeC, time/2);
   }
   //the function to remove the class
   function removeC(){
@@ -117,17 +141,6 @@ function boardCounter(counter){
   counterBoard.innerHTML = counter;
 }
 //a function to do the steps of the game
-//-ejecutar el readgamemachine por primera vez
-//esperar a que el jugador juegue
-//ir comparando la jugada del jugador con el correspondiente paso en el array de la máquina
-//si las jugadas son correctas aumentar el counter en uno y ejecutar readGameMachine
-/* un comparador:
-1. almacenar el botón apretado por el jugador en una variable
-2. usar el contador como length maximo
-document.addEventListener('click', function(e) {
-    alert(e.target.id);
-});
-*/
 
 function steps(){
   //add event listener to identify clicked button
@@ -148,56 +161,52 @@ function steps(){
       break;
     }
     //compare the button pressed to the array of the machine
-    comparePlays();
+    comparePlays(pushedBtn);
   });
 }
-//---necesito dos counters: 1 que es el que lleva todo el juego
-//--otro que indica hasta donde tengo que comparar el juego con la maquina, este counter siempre tiene que volver a 1
-// --el counter de todo el juego solo aumenta una vez se haya ejecutado todo el counter del comparador
-//tengo que volver a chequear desde el principio siempre
+
 //function to compare play of the player and array of the machine
-function comparePlays(){
+function comparePlays(pushedBtn){
+  //execute the sound and color of the button
+  playByNumber(pushedBtn);
   //if the button equals the machine advance one position
   if(pushedBtn === gameMachine[loop]){
     loop++;
-    console.log("fue igual");
     //see if the cycle is finished
     compareLength();
+  } else if(strictValue){
+    playSoundErr();
+    console.log("strict activado");
+    if (url.indexOf('?') !== -1){
+       url += 'param=1'
+       console.log(url);
+    }else{
+       url += '?param=1'
+       console.log(url);
+    }
+    window.location.href = url;
+    //window.setTimeout(function(){history.go(0)}, 1000);
+    //history.go(0);
+  }else{
+    //if not the same repeat the cycle and throw an error
+    playSoundErr();
+    loop = 0;
+    readGameMachine(counter);
   }
 }
 function compareLength(){
   //if loop less, continue comparing
-  console.log("estoy en compareLenght " + counterMachine);
   if(loop === counterMachine){
     counterMachine++;
     loop = 0;
-    console.log("se acabó este ciclo" + counterMachine + " loop vuelve a cero " + loop);
-
+    readGameMachine(counter);
   }
 }
 
-/*
-function comparePlays(playPlayer, arrMachine, lengthArr){
-  var loop = 0;
-  while(loop<=lengthArr){
-    if(playPlayer === arrMachine[loop]){
-      loop++;
-      console.log("loop adentro " + loop + " length " + lengthArr);
-    } else{
-      console.log("el array " + arrMachine[loop]);
-      break;
-    }
-  }
-
-  if(loop === lengthArr){
-    console.log("loop es " + loop);
-    return true;
-  }
-}
-*/
 //read the array from the machine and reproduce the buttons according to it
 function readGameMachine(counter){
   //iterate the function to traverse all the array
+  increaseSpeed(timer);
   if(counter<counterMachine){
     //establish a timeout
     window.setTimeout(function(){
@@ -209,19 +218,55 @@ function readGameMachine(counter){
       boardCounter(counter + 1);
       //add to the counter
       counter++;
-      console.log(numberMachine);
       //execute the same function with a new number
       readGameMachine(counter);
-    }, timer);
+    }, timerSpeed);
   }
 }
 
+//increase the speed when the counter increases
+function increaseSpeed(time){
+  //until step 7 goes slow, then increases
+  if(counterMachine<7){
+    return timerSpeed = timer;
+  }
+  if(counterMachine>=7 && counterMachine<12){
+    console.log("increase " + timerSpeed);
+    return timerSpeed = time/1.3;
+  } else if(counterMachine>=12){
+    console.log("increase " + timerSpeed);
+    return timerSpeed = time/1.6;
+  }
+}
 
 //start the game creating the array of the machine
-start.addEventListener("click", createGMachine);
+start.addEventListener("click", function(){
+  start.disabled = true;
+  start.classList.add("startDisabled");
+  createGMachine();
+});
 
 //prueba
 strict.addEventListener("click", function(){
-  readGameMachine(0);
+  console.log("activado el strict "+ url);
+  strictValue = true;
 });
-//steps();
+//restart the game
+reset.addEventListener("click", function(){
+  history.go(0);
+});
+/*
+var url = window.location.href;
+if (url.indexOf('?') !=== -1){
+   url += '&param=1'
+}else{
+   url += '?param=1'
+}
+window.location.href = url;
+*/
+window.onload = function(){
+  if (url.indexOf('param=1') !== -1){
+     createGMachine();
+     console.log(url);
+  }
+}
