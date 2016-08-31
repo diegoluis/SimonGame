ffa60e17f27696d0b772e7520ceca9ddd52a8e45
diegoluis <diego@diegoluis.net>
2016-08-31 19:11:14 +0200

    disabling of buttons working

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 /*
 
-- si son incorrectas, revisar si está el "strict mode" activado, si es así vuelve a empezar, si no es así, repite la secuencia.
 - cuando la máquina está jugando no permitir que los botones se puedan presionar por el jugador (ya está con start)
 -revisar el css (espacios, etc)
 //manejo de errores
@@ -10,7 +9,6 @@
 //-----------variables-----------//
 var url = window.location.href;
 var gameMachine = [];
-//var gamePlayer = [];
 //store the pushed button from player
 var pushedBtn;
 var number;
@@ -22,7 +20,7 @@ var green = document.getElementById("green");
 var red = document.getElementById("red");
 var yellow = document.getElementById("yellow");
 var blue = document.getElementById("blue");
-var allButtons = [green,red,yellow,blue];
+var allButtons = document.getElementsByClassName("square");
 //button container
 var boardInt = document.getElementById("boardInt");
 //audios
@@ -33,8 +31,7 @@ var audio4 = document.getElementById("s4");
 var audioErr = document.getElementById("sErr");
 //timer
 var timer = 500;
-//length of array
-var lengthArray;
+var pause = 500;
 // counter on the board
 var counterBoard = document.getElementById("counter");
 //variable to control the steps of the game
@@ -44,20 +41,15 @@ var counter = 0;
 var loop = 0;
 //variable to control the strict mode
 var strictValue = false;
-/*
-var greenGlow = document.getElementsByClassName("greenGlow");
-var redGlow = document.getElementsByClassName("redGlow");
-var yellowGlow = document.getElementsByClassName("yellowGlow");
-var blueGlow = document.getElementsByClassName("blueGlow");
-*/
+//variables to control human and machine
+var machine = "machine";
+var human = "human";
+//var to store a timeout
+//var timePause = window.setTimeout(function(){readGameMachine(counter);},pause);;
 //------------------------------//
 
 //create the random number and add it to an array
 function createGMachine(){
-  //gameMachine = [];
-  //counterMachine = 1;
-  //counter = 0;
-  //loop = 0;
   // generate the sequence of the game of the machine
   for(var i= 0; i<20; i++){
     //execute the random number creator
@@ -88,36 +80,64 @@ red.addEventListener("click", function(){playSound(audio2);});
 yellow.addEventListener("click", function(){playSound(audio3);});
 blue.addEventListener("click", function(){playSound(audio4);});
 
+
 //activate/deactivate the buttons
 ///NOT WORKING////
-function toggleButtons(action){
-  green.disabled = true;
-  allButtons.forEach(function(e){
-    e.disabled = action;
-    console.log(e);
-  })
+function toggleButtons(buttons, state, color){
+  for(var i = 0; i<buttons.length; i++){
+      buttons[i].disabled = state;
+  }
+  if(color ==! undefined){
+    color.disabled = false;
+  }
 }
+//toggleButtons(allButtons, true, "en compareLength");
+// en playByNumber identificar si el numero es de la maquina
 //---- reproduce automatically the array -------//
 timer = 1000;
 //associate each number with a button and a sound
-function playByNumber(number){
+function playByNumber(number, player){
   increaseSpeed(timer);
   switch (number) {
     case 1:
       playSound(audio1);
       changeClasses(green, "greenGlow", timer);
+      if(player === "machine"){
+        toggleButtons(allButtons, true, green);
+      }else{
+        toggleButtons(allButtons, false);
+      }
+
       break;
     case 2:
       playSound(audio2);
       changeClasses(red, "redGlow", timer);
+      if(player === "machine"){
+        toggleButtons(allButtons, true, red);
+      }else{
+        toggleButtons(allButtons, false)
+      }
+
       break;
     case 3:
       playSound(audio3);
       changeClasses(yellow, "yellowGlow", timer);
+      if(player === "machine"){
+        toggleButtons(allButtons, true, yellow);
+      }else{
+        toggleButtons(allButtons, false)
+      }
+
       break;
     case 4:
       playSound(audio4);
       changeClasses(blue, "blueGlow", timer);
+      if(player === "machine"){
+        toggleButtons(allButtons, true, blue);
+      }else{
+        toggleButtons(allButtons, false);
+      }
+
       break;
   }
 }
@@ -168,38 +188,36 @@ function steps(){
 //function to compare play of the player and array of the machine
 function comparePlays(pushedBtn){
   //execute the sound and color of the button
-  playByNumber(pushedBtn);
+  playByNumber(pushedBtn, human);
   //if the button equals the machine advance one position
   if(pushedBtn === gameMachine[loop]){
     loop++;
     //see if the cycle is finished
     compareLength();
   } else if(strictValue){
+    //if strict is activated play error
     playSoundErr();
-    console.log("strict activado");
-    if (url.indexOf('?') !== -1){
-       url += 'param=1'
-       console.log(url);
+    //add parameter to url
+    if (url.indexOf('param=1') === -1){
+       url += '?param=1';
     }else{
-       url += '?param=1'
-       console.log(url);
+       url += '?param=1';
     }
-    window.location.href = url;
-    //window.setTimeout(function(){history.go(0)}, 1000);
-    //history.go(0);
+    //reload the page with a pause
+    window.setTimeout(function(){window.location.href = url;},1000);
   }else{
     //if not the same repeat the cycle and throw an error
     playSoundErr();
     loop = 0;
-    readGameMachine(counter);
+    window.setTimeout(function(){readGameMachine(counter);},pause);
   }
 }
 function compareLength(){
-  //if loop less, continue comparing
+  //if loop less, continue comparing otherwise advance one step and play machine
   if(loop === counterMachine){
     counterMachine++;
     loop = 0;
-    readGameMachine(counter);
+    window.setTimeout(function(){readGameMachine(counter,toggleButtons(allButtons, false, "en compareLength set timeout"));},pause);
   }
 }
 
@@ -213,7 +231,7 @@ function readGameMachine(counter){
       //store the number from the array of the machine
       var numberMachine = gameMachine[counter];
       //play a button
-      playByNumber(numberMachine);
+      playByNumber(numberMachine, machine);
       //modify the counter according to the laps
       boardCounter(counter + 1);
       //add to the counter
@@ -221,6 +239,8 @@ function readGameMachine(counter){
       //execute the same function with a new number
       readGameMachine(counter);
     }, timerSpeed);
+  } else if(counter === counterMachine){
+    toggleButtons(allButtons, false);
   }
 }
 
@@ -231,10 +251,8 @@ function increaseSpeed(time){
     return timerSpeed = timer;
   }
   if(counterMachine>=7 && counterMachine<12){
-    console.log("increase " + timerSpeed);
     return timerSpeed = time/1.3;
   } else if(counterMachine>=12){
-    console.log("increase " + timerSpeed);
     return timerSpeed = time/1.6;
   }
 }
@@ -246,27 +264,39 @@ start.addEventListener("click", function(){
   createGMachine();
 });
 
-//prueba
+//activate strict mode
 strict.addEventListener("click", function(){
-  console.log("activado el strict "+ url);
-  strictValue = true;
+  //toggle the strict value on and off
+  if(strictValue){
+    strictValue = false;
+    strict.classList.remove("strictOn");
+  }else if(strictValue === false){
+    strictValue = true;
+    strict.classList.add("strictOn");
+  }
 });
 //restart the game
 reset.addEventListener("click", function(){
-  history.go(0);
+  window.location.href =  window.location.href.split("?")[0];
+
 });
-/*
-var url = window.location.href;
-if (url.indexOf('?') !=== -1){
-   url += '&param=1'
-}else{
-   url += '?param=1'
-}
-window.location.href = url;
-*/
+
 window.onload = function(){
+  toggleButtons(allButtons, true, "en onloadWindow");
   if (url.indexOf('param=1') !== -1){
      createGMachine();
+     strictValue = true;
+     start.disabled = true;
+     start.classList.add("startDisabled");
      console.log(url);
   }
-}
+  //check if strict is active to color the button accordingly
+  if(strictValue){
+    strict.classList.add("strictOn");
+  }else if(strictValue === false){
+    strict.classList.remove("strictOn");
+  }
+};
+
+//bug con strict la página se recarga pero reproduce dos vece el primer numero
+//desctivado por defecto, solo se activa cuando está jugando el jugador
